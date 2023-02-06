2acf70f12ffbef89e56c93d88bb8d7097e879905
Tris <tris@hivemind.me>
2023-02-06 15:21:23 +1100

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import axios from 'axios'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+
+
+export default async function asyncHome() {
+
+  const getToken = await axios
+  .get(
+    'https://divine-cosmopolitan-choice.discover.quiknode.pro/8e2568759967bf9240be97cc620357e38436ae4a/eth/v1/beacon/states/head/validators?status=pending_queued'
+  )
+  .catch(function (err) {
+    throw err
+  })
+console.log(getToken.data.data.length)
+console.log(getToken.data.data)
+
+const valsEntering = getToken.data.data.length
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
+        {/* <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
@@ -28,20 +43,22 @@ export default function Home() {
               priority
             />
           </a>
-        </div>
+        </div> */}
       </div>
 
       <div className={styles.center}>
-        <Image
+        {/* <Image
           className={styles.logo}
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
-        />
+        /> */}
+        <h2 className={inter.className}> Validators entering </h2>
         <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+          {/* <Image src="/thirteen.svg" alt="13" width={40} height={31} priority /> */}
+          <h1 className={inter.className}>{ valsEntering}</h1>
         </div>
       </div>
 
@@ -52,12 +69,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={inter.className}>
+          {/* <h2 className={inter.className}>
             Docs <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
             Find in-depth information about Next.js features and API.
-          </p>
+          </p> */}
         </a>
 
         <a
@@ -66,10 +83,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
+          {/* <h2 className={inter.className}>
+            Real data
+          </h2> */}
+          <p className={inter.className}>Real data - Directly from a node.</p>
         </a>
 
         <a
@@ -78,12 +95,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={inter.className}>
+          {/* <h2 className={inter.className}>
             Deploy <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          </p> */}
         </a>
       </div>
     </main>
